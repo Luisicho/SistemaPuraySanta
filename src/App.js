a7977e6e9abd369b50eeb43d85f00de6d5556bef
Comunicación DT <comunicaciondiocesisdetepic@MacBook-Pro-de-Comunicacion.local>
2026-05-06 10:32:29 -0700

    modificacion de ruta de carpeta

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CccBaseLine, ThemeProvider } from "@mui/material";
+
+
+function App() {
+  return (
+    <div className="app">
+
+    </div>
+  );
+}
+
+export default App;
