64230a777b70661d7fc71cd12f5771da964754bf
luisicho <luism18.1999@gmail.com>
2026-05-12 09:40:13 -0700

    create sidebar

--- src/App.js
+++ src/App.js
@@ -4,18 +4,40 @@ import { Routes, Route } from "react-router-dom";
 import CssBaseline from '@mui/material/CssBaseline';
 import Topbar from "./scenes/global/Topbar";
 import Dashboard from "./scenes/dashboard";
+import SidebarApp from "./scenes/global/SidebarApp"
+// import Team from "./scenes/team"
+// import Invoices from "./scenes/invoices"
+// import Contacts from "./scenes/contacts"
+// import Bar from "./scenes/bar"
+// import Form from "./scenes/form"
+// import Line from "./scenes/line"
+// import FAQ from "./scenes/faq"
+// import Geography from "./scenes/geography"
+// import Calendar from "./scenes/calendar"
 
 function App() {
   const   [theme, colorMode] = useMode();
+  
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <SidebarApp />
           <main className="content">
             <Topbar />
             <Routes>
               <Route path ="/" element={<Dashboard/>} />
+              {/* <Route path ="/team" elemeπnt={<Team/>} /> */}
+              {/* <Route path ="/contacts" element={<Contacts/>} /> */}
+              {/* <Route path ="/invoices" element={<Invoices/>} /> */}
+              {/* <Route path ="/form" element={<Form/>} /> */}
+              {/* <Route path ="/bar" element={<Bar/>} /> */}
+              {/* <Route path ="/pie" element={<Pie/>} /> */}
+              {/* <Route path ="/line" element={<Line/>} /> */}
+              {/* <Route path ="/faq" element={<FAQ/>} /> */}
+              {/* <Route path ="/geography" element={<Geography/>} /> */}
+              {/* <Route path ="/calendar" element={<Calendar/>} /> */}
             </Routes>
           </main>
         </div>
---
+++ src/scenes/global/SidebarApp.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import Link from "react-router-dom";
+import { token } from "../../theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlinedIcon from "@mui/icons-material/PieChartOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+
+const SidebarApp = () => {
+  const theme = useTheme();
+  const colors = token(theme.palette.colors);
+  const [isCollapsed, serIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+        },
+        "& .pro-icon-wrappor": {
+          backgroundColor: "transparent !important",
+        },
+        "& .pro-inner.item:hover": {
+          padding: "5px 35px 5px 20px !important",
+        },
+        "& .pro-menu-item:active": {
+          color: "#6870fa !important",
+        },
+      }}
+    >
+      <Sidebar>
+        <Menu>
+          <MenuItem>
+            <Box></Box>
+          </MenuItem>
+
+          {/* USER */}
+          {!isCollapsed && (
+            <Box mb="25px">
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <img
+                  alt="profile-user"
+                  width="100px"
+                  height="100px"
+                  src={`../../assets/user.png`}
+                  style={{ cursor: "pointer", borderRadius: "50%" }}
+                />
+              </Box>
+
+              <Box textAlign="center">
+                <Typography
+                  variant="h2"
+                  color={colors.grey[100]}
+                  fontWeight="bold"
+                  sx={{ m: "10px 0 0 0" }}
+                >
+                  Ed Roh
+                </Typography>
+                <Typography variant="h5" color={colors.greenAccent[500]}>
+                  VP Fancy Admin
+                </Typography>
+              </Box>
+            </Box>
+          )}
+        </Menu>
+      </Sidebar>
+    </Box>
+  );
+};
+
+export default SidebarApp;
