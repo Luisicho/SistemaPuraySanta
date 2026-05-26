6fc0f0705a103083c8db63b1758c2e4e546b748a
luisicho <luism18.1999@gmail.com>
2026-05-26 10:08:47 -0700

    Se creo Bar

---
+++ src/scenes/bar/index.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+import Header from "../components/Header";
+import BarChart from "../components/BarChart";
+
+const Bar = () => {
+  return (
+    <Box sx={{ m: "20px" }}>
+      <Header title="Bar Chart" subtitle="Estadisticas de barra" />
+      <Box sx={{ height: "75vh" }}>
+        <BarChart />
+      </Box>
+    </Box>
+  );
+};
+
+export default Bar;
