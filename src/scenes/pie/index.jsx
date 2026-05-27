f9feb09e302aa6a585ecb525784780e7452627c3
luisicho <luism18.1999@gmail.com>
2026-05-27 10:20:03 -0700

    Se creo pie Chart

---
+++ src/scenes/pie/index.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+import Header from "../components/Header";
+import PieChart from "../components/PieChart";
+
+const Pie = () => {
+  return (
+    <Box sx={{ m: "20px" }}>
+      <Header title="Pie Chart" subtitle="Estadisticas de pay" />
+      <Box sx={{ height: "75vh" }}>
+        <PieChart />
+      </Box>
+    </Box>
+  );
+};
+
+export default Pie;
