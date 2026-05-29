afe5ca73a788989fc42079a5005c12cee6f1e3cf
luisicho <luism18.1999@gmail.com>
2026-05-29 16:53:52 -0700

    Se creo segundo rengolon dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -73,6 +73,185 @@ const Dashboard = () => {
             }
           />
         </Box>
+        <Box
+          sx={{
+            gridColumn: "span 3",
+            backgroundColor: colors.primary[400],
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <StatBox
+            title="2,333"
+            subtitle="Emails Sent"
+            progress="0.95"
+            increase="+50%"
+            icon={
+              <EmailIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          sx={{
+            gridColumn: "span 3",
+            backgroundColor: colors.primary[400],
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <StatBox
+            title="15,4531"
+            subtitle="Emails Sent"
+            progress="0.55"
+            increase="+9%"
+            icon={
+              <EmailIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          sx={{
+            gridColumn: "span 3",
+            backgroundColor: colors.primary[400],
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <StatBox
+            title="1124,545"
+            subtitle="Emails Sent"
+            progress="0.25"
+            increase="+64%"
+            icon={
+              <EmailIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        {/* ROW 2 */}
+        <Box
+          sx={{
+            gridColumn: "span 8",
+            gridRow: "span 2",
+            backgroundColor: colors.primary[400],
+          }}
+        >
+          <Box
+            sx={{
+              mt: "25px",
+              p: "0 30px",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <Typography
+                sx={{
+                  variant: "h5",
+                  fontWeight: "600",
+                  color: colors.grey[100],
+                }}
+              >
+                Regreso Generado
+              </Typography>
+              <Typography
+                sx={{
+                  variant: "h3",
+                  fontWeight: "bold",
+                  color: colors.greenAccent[500],
+                }}
+              >
+                $285,235
+              </Typography>
+            </Box>
+            <Box>
+              <IconButton>
+                <DownloadOutlinedIcon
+                  sx={{ fontSize: "26px", color: colors.greenAccent[600] }}
+                />
+              </IconButton>
+            </Box>
+          </Box>
+          <Box sx={{ height: "250px", mt: "-20px" }}>
+            <LineChart isDashboard={true} />
+          </Box>
+        </Box>
+        {/* TRANSACTIONS */}
+        <Box
+          sx={{
+            gridColumn: "span 4",
+            gridRow: "span 2",
+            backgroundColor: colors.primary[400],
+            overflow: "auto",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              borderBottom: `4px solid ${colors.primary[100]}`,
+              p: "15px",
+            }}
+          >
+            <Typography
+              sx={{
+                color: colors.greenAccent[500],
+                variant: "h5",
+                fontWeight: "600",
+              }}
+            >
+              Transacciones recientes
+            </Typography>
+          </Box>
+          {mockTransactions.map((transaction, i) => (
+            <Box
+              key={`${transaction.txId}-${i}`}
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                borderBottom: `4px solid ${colors.primary[100]}`,
+                p: "15px",
+              }}
+            >
+              <Box>
+                <Typography
+                  sx={{
+                    color: colors.greenAccent[500],
+                    variant: "h5",
+                    fontWeight: "600",
+                  }}
+                >
+                  {transaction.txId}
+                </Typography>
+                <Typography sx={{ color: colors.grey[400] }}>
+                  {transaction.user}
+                </Typography>
+              </Box>
+
+              <Box sx={{ color: colors.grey[100] }}>{transaction.date}</Box>
+              <Box
+                sx={{
+                  backgroundColor: colors.greenAccent[500],
+                  p: "5px 10px",
+                  borderRadius: "4px",
+                }}
+              >
+                {transaction.cost}
+              </Box>
+            </Box>
+          ))}
+        </Box>
       </Box>
     </Box>
   );
